47b5e0f0a0b6a0029a2d1a8e6b30522941482609
a.pezzotta <a.pezzotta@criteo.com>
2022-08-23 14:46:03 +0200

    [power] Fix problems with the power button

--- x735-v2.5-scripts/x735pwr.sh
+++ x735-v2.5-scripts/x735pwr.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 SHUTDOWN=5
-REBOOTPULSEMINIMUM=200
-REBOOTPULSEMAXIMUM=600
 echo "$SHUTDOWN" > /sys/class/gpio/export
 echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
+
 BOOT=12
 echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
+
+REBOOTPULSEMINIMUM=200
+REBOOTPULSEMAXIMUM=600
 
 while [ 1 ]; do
   shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
@@ -20,7 +22,7 @@ while [ 1 ]; do
       /bin/sleep 0.02
       if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
         sudo /usr/bin/wall "Shutting down"
-        sudo /usr/sbin/shutdown -H now
+        sudo /usr/sbin/shutdown -h now
         exit
       fi
       shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
